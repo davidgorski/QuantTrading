4e1536a0ee0a9c9fc04841d703840547938997af
davidgorski <david.a.gorski@gmail.com>
2023-08-26 20:04:54 -0400

    Add runtimes and IS_PROD

--- src/main/scala/com/quantTrading/Settings.scala
+++ src/main/scala/com/quantTrading/Settings.scala
@@ -3,21 +3,59 @@ package com.quantTrading
 import com.quantTrading.dateUtils.DateUtils
 import com.quantTrading.dateUtils.DateUtils.MarketCalendar
 
-import java.time.{LocalDate, ZoneId}
-import java.util.concurrent.TimeUnit
-import scala.concurrent.duration.FiniteDuration
+import java.time.{LocalDate, ZoneId, ZonedDateTime}
+import scala.collection.mutable.{SortedSet => MutableSortedSet}
+import scala.collection.immutable.{SortedSet => ImmutableSortedSet}
 
 
 object Settings {
 
-  val LOOP_TIME: FiniteDuration = FiniteDuration(5, TimeUnit.SECONDS)
 
   val TIMEZONE: ZoneId = ZoneId.of("America/New_York")
 
   val ALPHAVANTAGE_API_KEY: String = sys.env("ALPHAVANTAGE_API_KEY")
 
+  val IS_PROD: Boolean = !System.getProperty("os.name").startsWith("Windows")
+
   val NYSE_CALENDAR: MarketCalendar = DateUtils.getNyseCalendar()
 
   val TODAY: LocalDate = DateUtils.getMostRecentBizdayIncludingToday(NYSE_CALENDAR)
 
+  val RUN_TIMES: ImmutableSortedSet[ZonedDateTime] = getRunTimes(TODAY)
+
+  private def getRunTimes(localDate: LocalDate): ImmutableSortedSet[ZonedDateTime] = {
+    val marketTimes: DateUtils.MarketTimes = NYSE_CALENDAR.sortedMap(localDate)
+    val open: ZonedDateTime = marketTimes.open
+    val close: ZonedDateTime = marketTimes.close
+
+    val runTimes: MutableSortedSet[ZonedDateTime] = MutableSortedSet[ZonedDateTime]()(Ordering.by(_.toEpochSecond))
+
+    // every thirty minutes until the close
+    var t: ZonedDateTime = open.plusMinutes(30L)
+    while (t.isBefore(close)) {
+      runTimes += t
+      t = t.plusMinutes(30L)
+    }
+
+    // every five minutes before the close beginning 30 minutes before
+    t = close.minusMinutes(30L)
+    while (t.isBefore(close)) {
+      runTimes += t
+      t = t.plusMinutes(5L)
+    }
+
+    // every 1 minute from -20 to close
+    t = close.minusMinutes(20L)
+    while (t.isBefore(close)) {
+      runTimes += t
+      t = t.plusMinutes(1L)
+    }
+
+    val result: ImmutableSortedSet[ZonedDateTime] = (
+      ImmutableSortedSet[ZonedDateTime]()(Ordering.by(_.toEpochSecond))
+        ++ runTimes
+    )
+
+    result
+  }
 }
