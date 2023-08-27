0cbc7753e1716a4e7557dd46758a34380cf9e90e
davidgorski <david.a.gorski@gmail.com>
2023-08-26 20:05:23 -0400

    Separate runner

--- src/main/scala/com/quantTrading/alphaVantage/AlphaVantage.scala
+++ src/main/scala/com/quantTrading/alphaVantage/AlphaVantage.scala
@@ -19,10 +19,6 @@ import java.time.LocalDate
 
 object AlphaVantage extends StrictLogging {
 
-  implicit val executionContext: ExecutionContextExecutor = ExecutionContext.fromExecutor(
-    new java.util.concurrent.ForkJoinPool(5) // Allow 10 requests to be executing at once
-  )
-
   case class AlphaVantageOhlcvBase(
     date: LocalDate,
     symbol: Symbol,
@@ -33,7 +29,7 @@ object AlphaVantage extends StrictLogging {
     volume: Option[PosZDouble]
   ) extends OhlcvBase
 
-  private def queryAndCheckAlphaVantage(
+  def queryAndCheckAlphaVantage(
     symbol: Symbol
   ): Validation[String, List[AlphaVantageOhlcvBase]] = {
 
@@ -109,91 +105,6 @@ object AlphaVantage extends StrictLogging {
       case e: Throwable =>
         Failure(e.toString)
     }
-  }
-
-  def main(args: Array[String]): Unit = {
-
-    val t0 = System.nanoTime()
-
-    val t1 = System.nanoTime()
-
-    val futures: Future[List[Validation[String, List[AlphaVantageOhlcvBase]]]] =
-      Future.sequence(Symbol.symbols.map(symbol => Future(queryAndCheckAlphaVantage(symbol))))
-
-    val queryResultFutures = Await.result(futures, Duration.Inf)
-    if (!queryResultFutures.forall(_.isSuccess)) {
-      val failures: List[String] = queryResultFutures.collect { case Failure(failureStr: String) => failureStr }
-      val failuresStr: String = failures.mkString(" | ")
-      throw new RuntimeException(failuresStr)
-    }
-    val queryResults: List[List[AlphaVantageOhlcvBase]] = queryResultFutures.collect { case Success(listOfAlphaVantageOhlcv: List[AlphaVantageOhlcvBase]) => listOfAlphaVantageOhlcv }
-
-    val logger: Logger = Logger(getClass.getName)
-    queryResults.foreach(queryResult => {
-      val ohlcvFirst = queryResult.minBy(_.date.toEpochDay)
-      logger.info(s"First ohlcv: ${ohlcvFirst.symbol} => ${ohlcvFirst.date}")
-    })
-
-    val ohlcByDateBySymbolMutable: MutableMap[LocalDate, MutableMap[Symbol, OhlcvBase]] = MutableMap[LocalDate, MutableMap[Symbol, OhlcvBase]]()
-    for (queryResult <- queryResults.flatten) {
-      if (!ohlcByDateBySymbolMutable.contains(queryResult.date))
-        ohlcByDateBySymbolMutable(queryResult.date) = MutableMap[Symbol, OhlcvBase]()
-      ohlcByDateBySymbolMutable(queryResult.date)(queryResult.symbol) = queryResult
-    }
-    val ohlcByDateBySymbol: SortedMap[LocalDate, ImmutableMap[Symbol, OhlcvBase]] =
-      SortedMap[LocalDate, ImmutableMap[Symbol, OhlcvBase]]()(Ordering.by(_.toEpochDay)) ++ ohlcByDateBySymbolMutable.map(kv => (kv._1, kv._2.toMap)).toMap
-
-    // instantiate strategies
-    val strategyMap = StrategyMap.getStrategyMap
-
-    // run strategies
-    val t2 = System.nanoTime()
-    for (date <- ohlcByDateBySymbol.keys) {
-      strategyMap.foreach { kv =>
-        val strategyName: String = kv._1
-        val strategy: Strategy[StrategyParamsBase, StrategyState] = kv._2
-        val strategyNew: Strategy[StrategyParamsBase, StrategyState] = strategy.onData(ohlcByDateBySymbol(date))
-        strategyMap(strategyName) = strategyNew
-      }
-    }
-    val t3 = System.nanoTime()
-
-    // print results and store them
-    strategyMap.keys.foreach { strategyName =>
-      val strategyResult = strategyMap(strategyName).state.getStrategyResult(ohlcByDateBySymbol)
-      val strategyResultSerializable: StrategyResultSerializable = StrategyResultSerializable(strategyResult)
-
-      println(strategyName)
-      println(strategyResult)
-      println(strategyResultSerializable)
-      println()
-    }
-
-    /*
-    val codecRegistry = fromRegistries(fromProviders(classOf[StrategyResultSerializable]), DEFAULT_CODEC_REGISTRY)
-    //XIT0VXT4RZKRUEHFbeyond
-    val mongoUri: String = "mongodb+srv://dgorski:XJNLm37RUtsasmEA@cluster0.wkwbxcv.mongodb.net/?retryWrites=true&w=majority"
-    val mongoClient: MongoClient = MongoClient(mongoUri)
-    val database: MongoDatabase = mongoClient.getDatabase("QuantLibWeb").withCodecRegistry(codecRegistry)
-    val collection: MongoCollection[StrategyResultSerializable] = database.getCollection("person")
-
-
-    val x: Seq[InsertOneResult] = collection.insertOne(strategyResultSerializable).results()
-
-
-    val y = 1
-    // val x: SortedMap[LocalDate, ImmutableMap[Symbol, Ohlcv]] =
-
-    // val f1 = Future(slowFunction(123))
-    //val f2 = Future(slowFunction(456))
-    //println(Await.result(f1, Duration.Inf))
-    //println(Await.result(f2, Duration.Inf))
-    val t4 = System.nanoTime()
-    */
-
-    println("time: " + ((t1 - t0) / 1e9).toString)
-    println("time: " + ((t2 - t1) / 1e9).toString)
-    println("time: " + ((t3 - t2) / 1e9).toString)
   }
 }
 
