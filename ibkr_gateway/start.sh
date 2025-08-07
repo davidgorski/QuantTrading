6c3839398f4188485dc7f9bf745682302aaa279b
davidgorski <david.a.gorski@gmail.com>
2025-08-06 20:57:21 -0400

    try running nginx and gateway together

--- ibkr_gateway/start.sh
+++ ibkr_gateway/start.sh
@@ -1,3 +1,5 @@
 cd gateway && sh bin/run.sh root/conf.yaml &
 cd webapp && python3 -m venv venv && . venv/bin/activate && venv/bin/pip install flask requests
-flask --app app run --debug -p 5056 -h 0.0.0.0
+flask --app app run --debug -p 5056 -h 0.0.0.0 &
+cron &
+nginx -g 'daemon off;'
