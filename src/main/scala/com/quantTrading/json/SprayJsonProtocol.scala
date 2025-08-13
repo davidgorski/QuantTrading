a3b417bcf2ea06599b1bf69c4538b8edf7b9125d
davidgorski <david.a.gorski@gmail.com>
2025-08-13 15:44:14 -0400

    add spray json protocol

---
+++ src/main/scala/com/quantTrading/json/SprayJsonProtocol.scala
@@ -0,0 +1,52 @@
+package com.quantTrading.json
+
+import org.scalactic.anyvals.{PosDouble, PosZDouble}
+import spray.json.{JsNumber, JsString, JsValue, JsonFormat, deserializationError}
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import com.quantTrading.symbols.Symbol
+
+object SprayJsonProtocol {
+
+  implicit val posZDoubleFormat: JsonFormat[PosZDouble] = new JsonFormat[PosZDouble] {
+
+    override def read(json: JsValue): PosZDouble = json match {
+      case JsNumber(value) => PosZDouble.from(value.doubleValue()).get
+      case other => deserializationError(s"Expected PosZDouble as JsNumber, got $other")
+    }
+
+    override def write(obj: PosZDouble): JsValue = JsNumber(obj.value)
+  }
+
+  implicit val zonedDateTimeFormat: JsonFormat[ZonedDateTime] = new JsonFormat[ZonedDateTime] {
+
+    private val formatter = DateTimeFormatter.ISO_ZONED_DATE_TIME
+
+    override def read(json: JsValue): ZonedDateTime = json match {
+        case JsString(str) => ZonedDateTime.parse(str, formatter)
+        case other => deserializationError(s"Expected ZonedDateTime as JsString, got $other")
+    }
+
+    override def write(zonedDateTime: ZonedDateTime): JsValue = JsString(zonedDateTime.format(formatter))
+  }
+
+  implicit val posDoubleFormat: JsonFormat[PosDouble] = new JsonFormat[PosDouble] {
+
+    override def read(json: JsValue): PosDouble = json match {
+      case JsNumber(value) => PosDouble.from(value.doubleValue()).get
+      case other => deserializationError(s"Expected PosDouble as JsNumber, got $other")
+    }
+
+    override def write(obj: PosDouble): JsValue = JsNumber(obj.value)
+  }
+
+  implicit val symbolFormat: JsonFormat[Symbol] = new JsonFormat[Symbol] {
+
+    override def read(json: JsValue): Symbol = json match {
+      case JsString(str) => Symbol.apply(str)
+      case other => deserializationError(s"Expected Symbol as JsString, got $other")
+    }
+
+    override def write(obj: Symbol): JsValue = JsString(obj.symbol)
+  }
+}
